bc84ee8f6190416db088efca221633feaaaeac73
mahesh.phalle <mahesh.phalle@outlook.com>
2024-12-12 21:44:11 +0530

    Added calculator class with arithmetic operations

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 import math
 
 class Calculator:
+
     def add(self, a, b):
         return a + b
 
@@ -13,23 +14,21 @@ class Calculator:
     def divide(self, a, b):
         return a / b
 
-    def devide(self, a, b):
-        if b == 0
-            raise ValueError("Cannot devide by zero.")
-        return a / b
-
-
-    # implement the square root function
-    def square_root(self, x):
-        return math.sqrt(x)
+    # TODO: Implement the following function to calculate the square root of a number.
+    # def square_root(self, x):
+    #     return math.sqrt(x)
 
 if __name__ == "__main__":
     calculator = Calculator()
+
     num1 = 16
     num2 = 4
+
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
-    num3 = 25
-    print(f"The square root of {num3} = {calculator.square_root(num3)}")
+
+    # TODO: Uncomment and test the square root feature.
+    # num3 = 25
+    # print(f"The square root of {num3} = {calculator.square_root(num3)}")
